2aa0db2dc8ca135802a69c903c099b43bf04295b
Virtus <lexuanhoang1997@gmail.com>
2018-01-09 22:48:12 +0700

    ServiceBuilding

---
+++ CDYNews.Service/ErrorLogService.cs
@@ -0,0 +1,40 @@
+﻿using CDYNews.Data.Infrastructure;
+using CDYNews.Data.Repositories;
+using CDYNews.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDYNews.Service
+{
+    public interface IErrorService
+    {
+        ErrorLog Create(ErrorLog error);
+
+        void Save();
+    }
+
+    public class ErrorLogService : IErrorService
+    {
+        private IErrorLogRepository _errorRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ErrorLogService(IErrorLogRepository errorLogRepository, IUnitOfWork unitOfWork)
+        {
+            this._errorRepository = errorLogRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public ErrorLog Create(ErrorLog error)
+        {
+            return _errorRepository.Add(error);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
---
+++ CDYNews.Service/PostService.cs
@@ -0,0 +1,89 @@
+﻿using CDYNews.Data.Infrastructure;
+using CDYNews.Data.Repositories;
+using CDYNews.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDYNews.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+
+        void Update(Post post);
+
+        void Delete(int id);
+
+        IEnumerable<Post> GetAll();
+
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryID, int page, int pageSize, out int totalRow);
+
+        Post GetById(int id);
+
+        IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow);
+
+        void SaveChange();
+    }
+
+    public class PostService : IPostService
+    {
+        private IPostRepository _postRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public PostService(IPostRepository postRepository, IUnitOfWork unitOfWork)
+        {
+            this._postRepository = postRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(Post post)
+        {
+            _postRepository.Add(post);
+        }
+
+        public void Delete(int id)
+        {
+            _postRepository.Delete(id);
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _postRepository.GetAll(new string[] { "PostCategory" });
+        }
+
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryID, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(s => s.Status && s.CategoryID == categoryID, out totalRow, page, pageSize, new string[] { "PostCatogory" });
+        }
+
+        public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
+        }
+
+        public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(s => s.Status, out totalRow, page, pageSize);
+        }
+
+        public Post GetById(int id)
+        {
+            return _postRepository.GetSingleById(id);
+        }
+
+        public void SaveChange()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Post post)
+        {
+            _postRepository.Update(post);
+        }
+    }
+}
